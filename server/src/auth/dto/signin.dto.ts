b943d193a82178b2072fb581d72a21861324a684
jaqb8 <jaqb8@users.noreply.github.com>
2023-06-28 16:27:01 +0000

    Deploying to gh-pages from @ jaqb8/streamer-spotlight@e2aa216b596ac09d04743b12df5507316fea2dd6 🚀

--- server/src/auth/dto/signin.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
-
-export class SignInDto {
-  @IsString()
-  username: string;
-
-  @IsString()
-  password: string;
-}
--- server/src/streamers/streaming-platform.enum.ts
+++
@@ -1,7 +0,0 @@
-export enum StreamingPlatform {
-  TWITCH = 'twitch',
-  YOUTUBE = 'youtube',
-  TIKTOK = 'tiktok',
-  KICK = 'kick',
-  RUMBLE = 'rumble',
-}
